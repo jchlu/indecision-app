870e62eb54316595169d617919a249e5fd85755c
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-11-04 17:29:14 +0000

    Initial install of Babel and plugins

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('app.js is running');
+
+// JSX - JavaScript XML
+var appRoot = document.getElementById('app');
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('app.js is running')
+
+// JSX - JavaScript XML
+var appRoot = document.getElementById('app')
+var template = <h1>Indecision App</h1>
+ReactDOM.render(template, appRoot)
