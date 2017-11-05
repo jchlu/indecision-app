6b235ce27a760c1f40e65e0b4def4403a4a07d4f
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-11-05 11:36:01 +0000

    After Challenge Two Injecting object properties into the template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,67 @@ console.log('app.js is running');
 
 // JSX - JavaScript XML
 var appRoot = document.getElementById('app');
+
+var app = {
+    title: 'Indecision App',
+    subTitle: 'Some Info on our App'
+};
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subTitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+var user = {
+    name: 'Johnny Choudhury-Lucas',
+    age: 46,
+    location: 'Bristol, England'
+};
+var userName = 'Johnny Choudhury-Lucas';
+var userAge = 46;
+var userLocation = 'Bristol, England';
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRo ot)
--- src/app.js
+++ src/app.js
@@ -2,5 +2,35 @@ console.log('app.js is running')
 
 // JSX - JavaScript XML
 var appRoot = document.getElementById('app')
-var template = <h1>Indecision App</h1>
+
+var app = {
+    title: 'Indecision App',
+    subTitle: 'Some Info on our App'
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subTitle}</p>
+        <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+        </ol>
+    </div>
+)
+var user = {
+    name: 'Johnny Choudhury-Lucas',
+    age: 46,
+    location: 'Bristol, England'
+}
+var userName = 'Johnny Choudhury-Lucas'
+var userAge = 46
+var userLocation = 'Bristol, England'
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
 ReactDOM.render(template, appRoot)
+// ReactDOM.render(templateTwo, appRo ot)
