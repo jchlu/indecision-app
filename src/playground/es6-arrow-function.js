0065d35020d1ab0e1806953054fe397f0b8040e6
Johnny Choudhury-Lucas <jchlu@me.com>
2017-11-18 15:40:08 +0000

    Demonstarate ES6 arrow functions Both the more verbose and short syntax versions

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -2,4 +2,31 @@ function square(x) {
     return x * x
 }
 
-console.log(square(8))
+const squareAnon = function(x) {
+    return x * x
+}
+
+/*
+// Verbose definition of an arrow function 
+const squareArrow = (x) => {
+    return x * x
+}
+*/
+
+const squareArrow = (x) => x * x
+
+/* console.log(square(8))
+console.log(squareAnon(3))
+console.log(squareArrow(9))
+ */
+
+ // Challenge:
+
+ const firstName = (fullName) => {
+     return fullName.split(' ')[0]
+ }
+
+ const firstNameShortSyntax = (fullName) => fullName.split(' ')[0]
+
+ console.log(firstName('Mike Smith'))
+ console.log(firstNameShortSyntax('John Smith'))
