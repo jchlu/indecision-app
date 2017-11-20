15c4b3ad07153a7bc1b49b233339e3f7258aeddd
Johnny Choudhury-Lucas <jchlu@me.com>
2017-11-20 21:23:56 +0000

    Flash out the random generator Here we add a random number generator and prevent the button for a decision being pressed if the length of the array is still zero.

--- src/app.js
+++ src/app.js
@@ -6,83 +6,56 @@ const appRoot = document.getElementById('app')
 const app = {
     title: 'Indecision App',
     subTitle: 'Some Info on our App',
-    options: [
-        "One",
-        "Two",
-    ]
+    options: [],
 };
 
-const addOne = () => {
-    count++;
-    console.log('addOne clicked', count);
-    renderCountApp();
+const formOnSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    renderApp();
 };
 
-const subtractOne = () => {
-    count--;
-    console.log('subtractOne clicked', count);
-    renderCountApp();
+const resetOptions = () => {
+    if (app.options.length > 0) {
+        app.options = [];
+        renderApp();
+    }
 };
 
-const resetCount = () => {
-    count = 0; 
-    console.log('reset clicked');
-    renderCountApp();
+function renderOptionsListItems() {
+    return app.options.map((option) => {
+        <li>{ option }</li>
+    });
 };
 
-let count = 0;
+const makeDecision = () => {
+    const randomNumber = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNumber];
+    alert(option);
+};
 
-const renderCountApp = () => {
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={subtractOne}>-1</button>
-            <button type="reset" onClick={resetCount}>reset</button>
+const renderApp = () => {
+    const template = (
+            <div>
+            <h1>{app.title}</h1>
+            { app.subTitle && <p>{app.subTitle}</p> }
+            <p> { app.options && app.options.length > 0 ? 'Here are your options' : 'No Options' } </p>
+            <button disabled={app.options.length === 0} onClick={makeDecision}>What should I do?</button>
+            <button onClick={resetOptions}>Remove All</button>
+            <ol>
+                {app.options.map((option) => <li key={option}>{ option }</li> )}
+            </ol>
+            <form onSubmit={formOnSubmit}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+            </form>
         </div>
     );
-    ReactDOM.render(templateThree, appRoot)
-};
+    ReactDOM.render(template, appRoot)
+}
 
-renderCountApp();
-
-/* Previous WIP below here */
-
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        { app.subTitle && <p>{app.subTitle}</p> }
-        <p> { app.options && app.options.length > 0 ? 'Here are your options' : 'No Options' } </p>
-        <ol>
-        <li>Item One</li>
-        <li>Item Two</li>
-        </ol>
-    </div>
-);
-
-const user = {
-    name: 'Johnny Choudhury-Lucas',
-    age: 46,
-    location: 'Bristol, England',
-};
-
-function getLocation(location) {
-    // Only render second value if the first value is thruthy
-    return location && <p>Location: {location}</p>
-    /* equavalent to if (location) {return <p>Location: {location}</p>} or,
-        return location ? <p>Location: {location}</p> : undefined */
-};
-
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {
-            (user.age && user.age >= 18) && <p>Age: {user.age}</p>
-            // (if both ^ are truthy) - then and only then, do ^
-        }
-        {getLocation(user.location)}
-    </div>
-);
-
-// ReactDOM.render(template, appRoot)
-// ReactDOM.render(templateTwo, appRoot)
+renderApp();
---
+++ src/playground/counter-and-previous-wip.js
@@ -0,0 +1,75 @@
+
+const addOne = () => {
+    count++;
+    console.log('addOne clicked', count);
+    renderCountApp();
+};
+
+const subtractOne = () => {
+    count--;
+    console.log('subtractOne clicked', count);
+    renderCountApp();
+};
+
+const resetCount = () => {
+    count = 0; 
+    console.log('reset clicked');
+    renderCountApp();
+};
+
+let count = 0;
+
+const renderCountApp = () => {
+    const templateThree = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={subtractOne}>-1</button>
+            <button type="reset" onClick={resetCount}>reset</button>
+        </div>
+    );
+    ReactDOM.render(templateThree, appRoot)
+};
+
+renderCountApp();
+
+/* Previous WIP below here */
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        { app.subTitle && <p>{app.subTitle}</p> }
+        <p> { app.options && app.options.length > 0 ? 'Here are your options' : 'No Options' } </p>
+        <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+        </ol>
+    </div>
+);
+
+const user = {
+    name: 'Johnny Choudhury-Lucas',
+    age: 46,
+    location: 'Bristol, England',
+};
+
+function getLocation(location) {
+    // Only render second value if the first value is thruthy
+    return location && <p>Location: {location}</p>
+    /* equavalent to if (location) {return <p>Location: {location}</p>} or,
+        return location ? <p>Location: {location}</p> : undefined */
+};
+
+const templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {
+            (user.age && user.age >= 18) && <p>Age: {user.age}</p>
+            // (if both ^ are truthy) - then and only then, do ^
+        }
+        {getLocation(user.location)}
+    </div>
+);
+
+// ReactDOM.render(template, appRoot)
+// ReactDOM.render(templateTwo, appRoot)
