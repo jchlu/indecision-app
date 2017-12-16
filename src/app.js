53e12c25eef7464b4d00df65d6c7d305105e5921
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-12-16 17:58:48 +0000

    Remove test code for  ES6 class properties

--- src/app.js
+++ src/app.js
@@ -5,24 +5,3 @@ import IndecisionApp from './components/IndecisionApp'
 import './styles/style.css'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
-
-class OldSyntax {
-  constructor () {
-    this.name = 'Mike'
-  }
-}
-
-const oldSyntax = new OldSyntax()
-console.log(oldSyntax)
-
-class NewSyntax {
-  name = "Jen"
-  getGreeting = () => {
-    return `Hi, my name is ${this.name}.`
-  }
-}
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newSyntax)
-console.log(newGetGreeting())
-
